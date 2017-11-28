4705cdfa830da25437b9d90d5c8800aa5fd44134
Jimmy Bergvall <den_dar_jimmy@hotmail.com>
2017-11-28 16:42:36 +0100

    testing Netlify

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 export const movieData = {
     title: "Star Wars - The Last Jedi",
-    imgUrl: "http://cdn.wegotthiscovered.com/wp-content/uploads/2017/09/the_last_jedi_by_dan_zhbanov-dbjoqc3.jpg",
+    imgUrl: "https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711",
     reviewtext: "Recension",
     actor: ["Luke Skywalker","Leia Organa"
 ]
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    devtool: 'source-map', // Detta skapar en bundle.js.map
-    entry: './code.js',  // Viktigt att det är "första" filen
+    devtool: "source-map", // Detta skapar en bundle.js.map
+    entry: "./code.js",  // Viktigt att det är "första" filen
     output: {
-        filename: 'distribution/bundle.js'
+        filename: "distribution/bundle.js"
     }
 };
